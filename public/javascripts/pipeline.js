72ee7e602c916386f0736d654292a59e59314cc0
Tiffany Luce <115090963+TiffanyLuce@users.noreply.github.com>
2022-10-12 14:49:00 +0200

    Update pipeline.js

--- public/javascripts/pipeline.js
+++ public/javascripts/pipeline.js
@@ -30,7 +30,7 @@ function PopulatePipelines(flavor) {
       var n = doc.name;
       if (n.toUpperCase().indexOf(filter) > -1) {
         if (doc.description == undefined) var row = `<tr><td onclick="PipelineDropdown('${n}')">${n}</td>`;
-        else var row = `<tr><td data-bs-toggle="tooltip" title="${doc.description}" onclick="PipelineDropdown('${n}')">${n}</td>`;
+        else var row = `<tr><td data-bs-toggle="tooltip" title=doc.description onclick="PipelineDropdown('${n}')">${n}</td>`;
         if (doc.status == 'active') {
           try{
             row += `<td>${doc.rate.toPrecision(3)}</td> <td>${(doc.dt || 0).toPrecision(1)}</td> <td>${doc.cycle-doc.error}</td>`;
