98071e9a75b09bfb154a9d14b5f677b484799651
Tiffany Luce <115090963+TiffanyLuce@users.noreply.github.com>
2022-10-20 11:58:17 +0200

    Update pipeline.js

--- public/javascripts/pipeline.js
+++ public/javascripts/pipeline.js
@@ -37,10 +37,9 @@ function PopulatePipelines(flavor) {
           }
           
           if (sensor != undefined) {
-            var row = ''; var descr;
-            $.getJSON(`/devices/sensor_detail?sensor=${sensor}`, sensordata => {
-              descr = sensordata['description'];
-              row += `<tr><td onclick="PipelineDropdown('${n}')">${descr}</td>`;
+            $.getJSON(`/devices/sensor_detail?sensor=${sensor}`, (thedata) => {
+              var descr = thedata['description'];
+              var row = `<tr><td onclick="PipelineDropdown('${n}')">${descr}</td>`;
               //if (doc.description == undefined) var row = `<tr><td onclick="PipelineDropdown('${n}')">${descr}</td>`;
               //else var row = `<tr><td title="${doc.description}" onclick="PipelineDropdown('${n}')">${descr}</td>`;
             });
@@ -373,4 +372,3 @@ function PipelineDropdown(pipeline) {
   });
   $('#pipelinebox').modal('show');
 }
-
