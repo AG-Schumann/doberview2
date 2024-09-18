f53395e9793cfffe60af98a130c5c21cc7c22faf
jarongrigat <--add>
2024-09-18 10:54:33 +0200

    get time from _id

--- routes/logs.js
+++ routes/logs.js
@@ -12,12 +12,12 @@ router.get('/get_logs', function(req, res) {
   var q = url.parse(req.url, true).query;
   var limit = 10000; // hard limit
   let tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  if(typeof q.limit != "undefined") {
+  if (typeof q.limit != "undefined") {
     limit = parseInt(q.limit);
   }
   var match = {};
   if (typeof q.from != 'undefined' && typeof q.to != 'undefined') {
-    match['date'] = {$gte:new Date(q.from), $lt:new Date(q.to)};
+    match['date'] = {$gte: new Date(q.from), $lt: new Date(q.to)};
   }
   if (typeof q.severity != 'undefined') {
     match['level'] = {$gte: parseInt(q.severity)};
@@ -29,9 +29,27 @@ router.get('/get_logs', function(req, res) {
     {$match: match},
     {$sort: {_id: -1}},
     {$limit: limit},
-    {$project: {name: 1, level: 1, msg: 1, funcname: 1, date: {$dateToString: {date: "$date", timezone:tz, format: "%Y-%m-%d %H:%M:%S.%L"}}, _id: 0}}
+    {
+      $project: {
+        name: 1,
+        level: 1,
+        msg: 1,
+        funcname: 1,
+        date: {
+          $dateToString: {
+            date: {$toDate: "$_id"},
+            timezone: tz,
+            format: "%Y-%m-%d %H:%M:%S"
+          }
+        },
+        _id: 0
+      }
+    }
   ]).then(docs => res.json(docs))
-  .catch(err => {console.log(err.message); return res.json([]);});
+      .catch(err => {
+        console.log(err.message);
+        return res.json([]);
+      });
 });
 
 module.exports = router;
