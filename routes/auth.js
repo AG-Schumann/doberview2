b0ef40da595884704092213b07c29c3fca75ef42
jarongrigat <jaron.grigat@googlemail.com>
2025-08-13 17:31:48 +0200

    fix logout

--- routes/auth.js
+++ routes/auth.js
@@ -35,4 +35,11 @@ router.get('/github/callback',
         });
     });
 
+router.post('/logout', function(req, res, next){
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
